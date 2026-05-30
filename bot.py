b61e8f23499f8b5a3fe23a4724e895c59a2cfdb9
abdusmal2 <abdusmal2@gmail.com>
2026-05-30 22:13:43 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -196,23 +196,11 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
             reply_markup=InlineKeyboardMarkup(keyboard)
         )
 
-        # CUSTOM PLAN
+    # CUSTOM PLAN
     elif query.data == "custom_plan":
 
-        cursor.execute(
-            "UPDATE users SET waiting_custom_plan=1 WHERE user_id=?",
-            (query.from_user.id,)
-        )
-
-        conn.commit()
-
         await query.message.reply_text(
-            "📝 Custom VIP Plan\n\n"
-            "Send the number of months you want.\n\n"
-            "Examples:\n"
-            "3\n"
-            "6\n"
-            "12"
+            "TEST CUSTOM PLAN WORKS"
         )
 
         # MANUAL PAYMENT
