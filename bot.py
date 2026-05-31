00fe53a79e7574e74ba8ea1615eddec9c33c8be1
abdusmal2 <abdusmal2@gmail.com>
2026-05-31 22:48:27 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -375,7 +375,7 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         from datetime import datetime, timedelta
 
-                user_id = int(query.data.split("_")[1])
+        user_id = int(query.data.split("_")[1])
 
         cursor.execute(
             """
