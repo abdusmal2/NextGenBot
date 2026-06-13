049cf8c870ba474a24c5c5847e4ce893ecd263a4
abdusmal2 <abdusmal2@gmail.com>
2026-06-13 18:14:35 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -676,17 +676,17 @@ async def receipt_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     ]
 
     msg = await update.message.reply_text(
-    "✅ Receipt received.\n\n"
-    "Click the button below to notify admin.",
-    reply_markup=InlineKeyboardMarkup(keyboard)
-)
+        "✅ Receipt received.\n\n"
+        "Click the button below to notify admin.",
+        reply_markup=InlineKeyboardMarkup(keyboard)
+    )
 
-cursor.execute(
-    "INSERT INTO user_messages VALUES (?, ?)",
-    (user.id, msg.message_id)
-)
+    cursor.execute(
+        "INSERT INTO user_messages VALUES (?, ?)",
+        (user.id, msg.message_id)
+    )
 
-conn.commit()
+    conn.commit()
 # CUSTOM PLAN HANDLER
 async def custom_plan_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
